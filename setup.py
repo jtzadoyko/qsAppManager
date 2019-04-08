f79e8d6c65d23d32aaeb5291808f912e23e6416e
jtzdoyko <44326766+jtzadoyko@users.noreply.github.com>
2019-04-07 22:12:08 -0400

    cleaning up names of files and folders

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 # @Author: jtzadoyko
 # @Date:   2019-03-09 23:19:56
 # @Last Modified by:   jtzadoyko
-# @Last Modified time: 2019-04-06 22:05:16
+# @Last Modified time: 2019-04-07 22:08:50
 
 
 from distutils.core import setup
@@ -12,6 +12,6 @@ setup(name='Master Items Manager',
       description='Library to create and maintain a qlik application',
       author='SDG Group USA',
       author_email='joshua.zadoyko@sdggroup.com and kevin.doyle@sdggroup.com',
-      url='NA',
+      url='https://github.com/jtzadoyko/qsAppManager',
       packages=[''],
      )
