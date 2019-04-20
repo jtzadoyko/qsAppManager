eae8aa2fc3cd51efef268252f22390bf0b2b4871
jtzadoyko <jtzadoyko@gmail.com>
2019-04-20 17:29:25 -0400

    updating relative imports

--- qsAppManager/api/__init__.py
+++ qsAppManager/api/__init__.py
@@ -3,3 +3,10 @@
 # @Date:   2019-03-09 23:20:07
 # @Last Modified by:   jtzadoyko
 # @Last Modified time: 2019-03-09 23:20:07
+
+
+from application import qsEngineAppApi
+from callback_pipeline import callback_pipeline
+from connection import qsEngineCommunication
+from generic import qsEngineGenericObjectApi
+from global import qsEngineGlobalApi
--- qsAppManager/configure/__init__.py
+++ qsAppManager/configure/__init__.py
@@ -3,3 +3,6 @@
 # @Date:   2019-03-09 23:20:07
 # @Last Modified by:   jtzadoyko
 # @Last Modified time: 2019-03-09 23:20:07
+
+from base import baseConfig
+from excel import excelConfig
