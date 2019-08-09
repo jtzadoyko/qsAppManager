2113c3cf42cc1a3b539d81ac36f934f8d069943f
Queenolin <49482085+Queenolin@users.noreply.github.com>
2019-08-09 13:59:52 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(name='Master Items Manager',
       version='1.0',
       description='Library to create and maintain a qlik application',
       author='SDG Group USA',
-      author_email='joshua.zadoyko@sdggroup.com and kevin.doyle@sdggroup.com',
+      author_email='joshua.zadoyko@sdggroup.com and kevin.doyle@sdggroup.com and lin.wang@sdggroup.com',
       url='https://github.com/jtzadoyko/qsAppManager',
       packages=[''],
      )
